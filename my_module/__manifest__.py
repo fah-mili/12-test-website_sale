804a29fc6e204a8078a6d25dbabe4c1792b631ae
fah <fah@odoo.com>
2019-03-15 15:02:01 +0100

    New module, retry

--- my_module/__manifest__.py
+++ my_module/__manifest__.py
@@ -20,12 +20,19 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['website_sale'],
+    'depends': [
+                # 'base',
+                'website_sale'
+               ],
 
     # always loaded
     'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
+        # 'demo/demo.xml',
     ],
 }
