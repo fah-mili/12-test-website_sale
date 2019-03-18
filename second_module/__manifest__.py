9eb21bc3e17027c61af4677e460b7490c2fc8c98
fah <fah@odoo.com>
2019-03-18 07:30:33 +0100

    Test with the customer module

---
+++ second_module/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Cytosurge Fix Tests',
+    'description': """
+        Cytosurge Tests
+    """,
+    'depends': ['website_sale'],
+    'data': [
+    ],
+    'installable': True,
+}
